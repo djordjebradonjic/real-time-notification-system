cbf346fad37ffbe82f6aac87d37d23ff48eabd18
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-06-27 13:47:55 +0200

    first commit

---
+++ src/main/java/com/djordje/realtime_notifications/RealtimeNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.djordje.realtime_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeNotificationsApplication.class, args);
+	}
+
+}
