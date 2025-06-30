bcda4c65496c26d703521f64bd0fc78678187e50
djordjebradonjic <djordjebradonjic99@gmail.com>
2025-06-30 15:24:31 +0200

    Create User Repository

---
+++ src/main/java/com/djordje/realtime_notifications/model/User.java
@@ -0,0 +1,34 @@
+package com.djordje.realtime_notifications.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(unique = true)
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
+    private String email;
+
+}
